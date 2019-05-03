470e018494f22a26f821d690cc539c7bd35ff043
Victorya Guncheva <victorya.guncheva@technofy.io>
2019-05-03 13:07:22 +0300

    fixes

---
+++ PhpAndMySQLInAWS/js/main.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function( $ ) {
+
+    $(document).on('click', '.menu-has-children i', function(e) {
+        $(this).next().toggleClass('menu-item-active');
+        $(this).nextAll('ul').eq(0).slideToggle();
+        $(this).toggleClass("fa-chevron-up fa-chevron-down");
+    });
+
+    $('.nav-menu a').on('click', function() {
+        // if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            if (target.length) {
+                var top_space = 0;
+
+                if ($('#header').length) {
+                    top_space = $('#header').outerHeight();
+
+                    if( ! $('#header').hasClass('header-fixed') ) {
+                        top_space = top_space - 20;
+                    }
+                }
+
+                if ($(this).parents('.nav-menu').length) {
+                    $('.nav-menu .menu-active').removeClass('menu-active');
+                    $(this).closest('li').addClass('menu-active');
+                }
+                return false;
+            }
+        // }
+    });
+});
